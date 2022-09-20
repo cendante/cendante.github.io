8316cbcbf4f5078ade85bc06cb2bb3fef2d7f8fa
cendante <antonio.cendanteijeiro@amey.co.uk>
2022-09-20 20:58:13 +0100

    commit1

---
+++ data.js
@@ -0,0 +1,52 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1_dgj8el2wntlicscjwf4dbq",
+      "name": "1_dgJ8el2wNtlICSCJwF4dbQ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -0.36087195279101536,
+        "pitch": 0.1633758323688852,
+        "fov": 1.1989879464110744
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.36087195279101536,
+          "pitch": 0.1633758323688852,
+          "rotation": 0,
+          "target": "0-1_dgj8el2wntlicscjwf4dbq"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.673905451644556,
+          "pitch": -0.1804253602593917,
+          "title": "Note",
+          "text": "Airbus A350"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
